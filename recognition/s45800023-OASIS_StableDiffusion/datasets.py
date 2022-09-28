36b2040e37f5c6cd7f4ec634706297203c8f9e69
Jacob Barrie <barriejj01@gmail.com>
2022-09-28 14:52:10 +1000

    Started working on datasets.py, installing required packages and set up cuda on local device.

--- recognition/s45800023-OASIS_StableDiffusion/datasets.py
+++ recognition/s45800023-OASIS_StableDiffusion/datasets.py
@@ -2,5 +2,64 @@
 """
 Created on Tue Sep 27 12:11:40 2022
 
+Script for pre-processing and cleaning of OASIS brain dataset. 
+
 @author: Jacob Barrie: s45800023
 """
+
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import color
+import os
+import cv2
+
+"""
+Code Block for sanity checking CUDA and setting the device. 
+
+"""
+print("Is device available: ",torch.cuda.is_available())
+print("Current Device: ",torch.cuda.current_device())
+print("Name: ",torch.cuda.get_device_name(0))
+
+device = torch.cuda.device(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
